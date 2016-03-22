aff25f5496e77e08f73b8523bed53d2a53857f6e
George <gdaviesdev@gmail.com>
2016-03-22 14:14:29 +0000

    Refactored code. Removed creation of text file for gateway.

--- PlexVPNDelete.sh
+++ PlexVPNDelete.sh
@@ -4,14 +4,12 @@ host -t a plex.tv | awk '{print $4}' | egrep ^[1-9] > ~/Library/Application\ Sup
 # Assign IPs to array called addresses
 IFS=$'\n' read -d '' -r -a addresses < ~/Library/Application\ Support/Plex\ Media\ Server/PlexIPs.txt
 
-# Assign default gateway to text file in Library
-netstat -nr | grep -w 'default' | awk '{print $2}' > ~/Library/Application\ Support/Plex\ Media\ Server/GatewayIP.txt
-
-# Assign Gateway IP to array called Gateway
-IFS=$'\n' read -d '' -r -a gateway < ~/Library/Application\ Support/Plex\ Media\ Server/GatewayIP.txt
+# Assign default gateway to variable called gateway
+gateway=`netstat -nr | grep -w 'default' | awk '{print $2}'`
+echo $gateway
 
 # Add routes
 for i in ${addresses[@]}
 do
-sudo route delete $i ${gateway[0]}
+sudo route delete $i $gateway
 done
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This OS X application/script adds Plex.tv IP addresses to your routes so that they do not go through your VPN. This is useful for when your VPN provider does not allow port forwarding, or it is not working correctly.
 
 How to Use
-Run the ‘PlexVPNScript’ application and you will be prompted for your password. You should then see confirmation that the routes have been added.
+Run the ‘PlexVPN’ application (PlexVPN.app/Contents) and you will be prompted for your password. You should then see confirmation that the routes have been added.
 
 There is also a script available if you prefer, just open Terminal and drag the script in to the Terminal window. Press enter and again you will be prompted for your password. You should then see confirmation of some IPs being added.
 
